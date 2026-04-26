ffc4884fb35075bf4fa68e14bbf75354dd034749
BUMA555 <144307922+BUMA555@users.noreply.github.com>
2026-04-26 21:17:19 +0800

    Prepare API for Koyeb deployment

--- apps/api/app/db/session.py
+++ apps/api/app/db/session.py
@@ -12,7 +12,7 @@ connect_args = {}
 if settings.database_url.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(settings.database_url, future=True, connect_args=connect_args)
+engine = create_engine(settings.sqlalchemy_database_url, future=True, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False, class_=Session)
 Base = declarative_base()
 
